94087b36f6d5c0c7b75870d9c15be286c00efd71
szewczyk.mira <szewczyk.mira@gmail.com>
2015-10-19 21:16:27 +0200

    4th almost done

--- Zestaw2/lista2.ml
+++ Zestaw2/lista2.ml
@@ -14,3 +14,22 @@ let z3 f l =
  in z33 f l []
 
 let rec z31 f l = if l = [] then [] else (z31 f (List.tl l))@[f (List.hd l)] 
+
+(* Zadanie 4 *)
+let rec merge cmp l1 l2 =
+  match l1, l2 with
+  [], _ -> l2
+  | _, [] -> l1
+  | h1::t1, h2::t2 -> (match cmp h1 h2 with
+    | true -> h1 :: merge cmp t1 l2
+    | _ -> h2 :: merge cmp l1 t2)
+
+let tail_merge cmp l1 l2 = 
+  let rec tail_acc cmp l1 l2 acc = 
+    match l1, l2 with
+    [], _ -> acc@l2
+    | _, [] -> acc@l1
+    | h1::t1, h2::t2 -> (match cmp h1 h2 with
+      | true -> tail_acc cmp t1 l2 (acc@[h1])
+      | _ -> tail_acc cmp l1 t2 (acc@[h2]))
+  in tail_acc cmp l1 l2 []
